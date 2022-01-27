f16f1e1fce8b855a60b716445ec9c7cd202a4ce1
oiseau92 <eniseagca@gmail.com>
2022-01-27 17:20:24 +0300

    adding-noti-page

--- Screens/Notification.js
+++ Screens/Notification.js
@@ -1,95 +1,93 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
-  NativeBaseProvider,
-  Center,
   Text,
-  Input,
-  Button,
   Box,
   HStack,
   Pressable,
   Image,
   FlatList,
   VStack,
-  Touchable
+  Spacer
 } from 'native-base'
 
-const Notification = () => {
-  const [selected, setSelected] = useState(0)
-  const [haber, setHaber] = useState([])
-
-  function getHaber() {
-    fetch('https://api.collectapi.com/news/getNews?country=tr&tag=general', {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        authorization: 'apikey 18Y3OfM83FFy2297uZRnDO:1yJshZKf8QLsHaA4rcYTzr'
-      }
-    })
-      .then((res) => {
-        return res.json()
-      })
-      .then((res) => {
-        console.log(res.success)
-        setHaber(res.result)
-      })
-  }
-
-  useEffect(() => {
-    getHaber()
-  }, [])
+const Notification = (props) => {
+  const { navigation } = props
+  const data = [
+    {
+      id: '1',
+      name: 'noti1',
+      image: require('../img/noti1.png'),
+      desc: 'Course succesfully enrolled',
+      time: '17:00'
+    },
+    {
+      id: '2',
+      name: 'noti2',
+      image: require('../img/noti1.png'),
+      desc: 'Thank you for purchasing',
+      time: '16:00'
+    },
+    {
+      id: '3',
+      name: 'noti3',
+      image: require('../img/noti2.png'),
+      desc: '3D Design Basic(NEW)',
+      time: '15:00'
+    },
+    {
+      id: '4',
+      name: 'noti4',
+      image: require('../img/noti3.png'),
+      desc: 'Discount 50% available !',
+      time: '14:00'
+    },
+    {
+      id: '5',
+      name: 'noti5',
+      image: require('../img/noti4.png'),
+      desc: 'Characters Animation (NEW)',
+      time: '13:00'
+    }
+  ]
 
   return (
-    <Box flex="1">
-      <HStack space={10}>
-        <Image source={require('../icon/home-page.png')} size={'xs'} />
-        <Text>Notification</Text>
-      </HStack>
-
+    <Box>
+      <Box w="100%" h="20" bgColor={'white'}>
+        <HStack>
+          <Pressable onPress={() => navigation.navigate('home')}>
+            <Image
+              ml="5"
+              source={require('../icon/home-page.png')}
+              w="10"
+              h="10"
+              alt="home"
+            ></Image>
+          </Pressable>
+          <Text fontSize="24" fontWeight={'bold'} ml="10">
+            Notification
+          </Text>
+        </HStack>
+      </Box>
       <FlatList
-        data={haber}
-        keyExtrator={(item, index) => index}
-        renderItem={({ item, index }) => (
-          <Box key={index}>
-            <HStack space={3} justifyContent={'space-between'}>
-              <Image
-                source={{ uri: item.image }}
-                style={{ width: 48 }}
-                alt="resim"
-              />
-              <VStack>
-                <Text>{item.name}</Text>
-                <Text>{item.description}</Text>
-              </VStack>
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Box w="100%" h="90" pl="5" key={item} mb="3" bgColor={'white'}>
+            <HStack>
+              <Image source={item.image} w="90" h="90" alt="notif"></Image>
+              <Spacer />
+              <Box w="290" h="90">
+                <VStack>
+                  <Text fontSize="20" fontWeight={'bold'}>
+                    {item.desc}
+                  </Text>
+                  <Text color="gray.500">{item.time}</Text>
+                </VStack>
+              </Box>
             </HStack>
           </Box>
         )}
       />
-
-      <Box flex={1} bg="white">
-        <Pressable onPress={() => true}>
-          <Box>
-            <Image
-              size="8"
-              source={require('../icon/home-page.png')}
-              mb="1"
-              alt="home"
-            />
-            <Text color="indigo.600">Home</Text>
-          </Box>
-        </Pressable>
-        <Pressable onPress={() => true}>
-          <Box>
-            <Image
-              size="8"
-              source={require('../icon/home-page.png')}
-              mb="1"
-              alt="home"
-            />
-            <Text color="indigo.600">Home</Text>
-          </Box>
-        </Pressable>
-      </Box>
     </Box>
   )
 }
