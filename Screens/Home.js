63fd67db92e4922562f79ab12aa58a8fcba3bcc9
oiseau92 <eniseagca@gmail.com>
2022-01-28 14:02:21 +0300

    home-update

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
   Box,
   HStack,
@@ -8,12 +8,43 @@ import {
   Link,
   SearchIcon,
   Button,
-  Pressable
+  Pressable,
+  FlatList,
+  VStack,
+  Spacer
 } from 'native-base'
+const Fav = (props) => {
+  const { isFav } = props
+  if (isFav) {
+    return <Image source={require('../img/fav.png')} alt="fav" w="6" h="6" />
+  } else {
+    return (
+      <Image source={require('../img/notfav.png')} alt="notfav" w="6" h="6" />
+    )
+  }
+}
 
 const Home = (props) => {
   const { navigation } = props
 
+  const data = [
+    {
+      id: '1',
+      image: require('../img/noti2.png'),
+      desc: '3D Design Basic',
+      star: '4',
+      isFav: false,
+      price: '$24,99'
+    },
+    {
+      id: '2',
+      image: require('../img/noti4.png'),
+      desc: 'Characters Animation',
+      star: '5',
+      isFav: true,
+      price: '$22,69'
+    }
+  ]
   return (
     <Box flex={1}>
       <HStack justifyContent={'space-around'} my="4">
@@ -67,8 +98,8 @@ const Home = (props) => {
           />
         </Box>
       </Pressable>
-      <HStack justifyContent={'space-between'} mx="5">
-        <Text fontSize="18">All courses</Text>
+      <HStack justifyContent={'space-between'} mx="5" mb="2">
+        <Text fontSize="18">Awesome courses</Text>
         <Link fontSize="18" href="see.com">
           See all
         </Link>
@@ -95,29 +126,51 @@ const Home = (props) => {
         </Button>
       </HStack>
       <Box>
-        <Box
-          w="80%"
-          h="56%"
-          ml="10"
-          bgColor={'blue.200'}
-          safeAreaTop
-          mt={'1'}
-        ></Box>
+        <Box w="80%" h="54%" ml="10" safeAreaTop mt={'1'}>
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <Box my="3">
+                <HStack>
+                  <Image source={item.image} w="70" h="70" alt="course" />
+                  <Box ml="5" w="150">
+                    <VStack>
+                      <Text mb="2" fontWeight="bold" fontSize="14">
+                        {item.desc}
+                      </Text>
+                      <HStack>
+                        <Image source={require('../img/star.png')} alt="star" />
+                        <Text pl="2">{item.star}</Text>
+                      </HStack>
+                    </VStack>
+                  </Box>
+                  <Box ml="6">
+                    <VStack>
+                      <Fav isFav={item.isFav} mb="2" />
+                      <Text color="#4a56fe">{item.price}</Text>
+                    </VStack>
+                  </Box>
+                </HStack>
+              </Box>
+            )}
+          />
+        </Box>
 
-        <Box h="8" mx="5" safeAreaBottom>
+        <Box h="12" mx="5" safeAreaBottom>
           <HStack justifyContent={'space-around'}>
             <Pressable onPress={() => navigation.navigate('home')}>
-              <Box w="8" h="8">
+              <Box w="20" h="9">
                 <Image
                   w="100%"
                   h="100%"
-                  source={require('../icon/home-page.png')}
+                  source={require('../icon/home.png')}
                   alt="home"
                 />
               </Box>
             </Pressable>
             <Pressable>
-              <Box w="8" h="8">
+              <Box w="9" h="9">
                 <Image
                   w="100%"
                   h="100%"
@@ -127,21 +180,21 @@ const Home = (props) => {
               </Box>
             </Pressable>
             <Pressable>
-              <Box w="8" h="8">
+              <Box w="9" h="9">
                 <Image
                   w="100%"
                   h="100%"
-                  source={require('../icon/mail.png')}
+                  source={require('../icon/message.png')}
                   alt="mail"
                 />
               </Box>
             </Pressable>
             <Pressable>
-              <Box w="8" h="8">
+              <Box w="9" h="9">
                 <Image
                   w="100%"
                   h="100%"
-                  source={require('../icon/user.png')}
+                  source={require('../icon/profile.png')}
                   alt="profile"
                 />
               </Box>
